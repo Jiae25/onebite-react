49a475127540362794c04a9b63b45905720a1c36
ahnjiae <asj8839@naver.com>
2024-03-31 23:50:01 +0900

    ch15. 객체 1

---
+++ section01/chapter15.js
@@ -0,0 +1,39 @@
+// 1. 객체 생성
+let obj1 = new Object(); // 객체 생성자
+let obj2 = {}; // 객체 리터럴 (대부분 사용)
+
+// 2. 객체 프로퍼티 (객체 속상)
+// key : value
+let person = {
+  name: "이정환",
+  age: 27,
+  hobby: "테니스",
+  job: "FE Developer",
+  extra: {},
+  10: 20,
+  "like cat": true,
+};
+
+// 3. 객체 프로퍼티를 다루는 방법
+// 3.1 특정 프로퍼티에 접근하는 방법 (점 표기법, 괄호 표기법)
+let name = person.name; // 존재하지 않는 프로퍼티에 접근하려고 하면 Undefined 값이 나온다
+let age = person["age"]; // key를 쌍따옴표와 함께 문자열로 작성, 쌍타옴표가 없으면 변수로 인식함
+
+let property = "hobby";
+let hobby = person[property];
+
+// 3.2 새로운 프로퍼티를 추가하는 방법
+person.job = "fe developer";
+person["favoriteFood"] = "떡볶이";
+
+// 3.3 프로퍼티를 수정하는 방법
+person.job = "educator";
+person["favoriteFood"] = "초콜릿";
+
+// 3.4 프로퍼티를 삭제하는 방법
+delete person.job;
+delete person["favoriteFood"];
+
+// 3.5 프로퍼티의 존재 유무를 확인하는 방법 (in 연산자)
+let result1 = "name" in person; // true
+let result2 = "cat" in person; // false
