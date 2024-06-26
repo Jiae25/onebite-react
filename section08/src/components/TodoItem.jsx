21d9ed1259fd091cb6ccd821d8480fdeb5223f2e
ahnjiae <asj8839@naver.com>
2024-06-27 00:02:02 +0900

    TodoItem.jsx 오타 수정

--- section08/src/components/TodoItem.jsx
+++ section08/src/components/TodoItem.jsx
@@ -4,7 +4,7 @@ const TodoItem = () => {
   return <div className="TodoItem">
     <input type="checkbox" />
     <div className="content">Todo...</div>
-    <div className="date">Datd</div>
+    <div className="date">Date</div>
     <button>삭제</button>
   </div>
 };
